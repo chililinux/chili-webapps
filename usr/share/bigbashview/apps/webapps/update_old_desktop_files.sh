55a28f5fafa17daddae7b441924bda7465174e11
vcatafesta <vcatafesta@gmail.com>
2025-03-20 14:29:51 -0400

    build-package new release: qui 20 mar 2025 14:29:48 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- usr/share/bigbashview/apps/webapps/update_old_desktop_files.sh
+++ usr/share/bigbashview/apps/webapps/update_old_desktop_files.sh
@@ -8,52 +8,53 @@ defaultBrowser=$(/usr/share/bigbashview/apps/webapps/check_browser.sh --default)
 
 # Iterate over all webapps
 for file in *webapp-biglinux*; do
-	if [[ "$file" = '*webapp-biglinux*' ]]; then
-		exit
-	fi
 
-	# Clean info before verify next file
-	unset browser name url icon categories profile
+    if [[ "$file" = '*webapp-biglinux*' ]]; then
+        exit
+    fi
 
-	# Read any file and get the name, url, icon and categories
-	while IFS= read -r line; do
-		case $line in
-		"Name="*)
-			name=${line#*Name=}
-			;;
-		"Exec="*)
-			browser=${line%% *}
-			browser=${browser#Exec=}
-			if [[ "$browser" =~ biglinux-webapp ]]; then
-				browser=$defaultBrowser
+    # Clean info before verify next file
+    unset browser name url icon categories profile
 
-			# In older versions of biglinux-webapp, Firefox and browsers based in Firefox use script in ~/.local/bin
-			elif [[ "$browser" =~ \.local/bin ]]; then
-				oldScript=$browser
-				browser=$(grep -m1 '\--new-instance' "$oldScript" | grep -o '^[^ ]*')
-				url=$(grep -m1 '\--new-instance' "$oldScript" | grep -o 'http[^" ]*')
-				profile='Default'
-			fi
+    # Read any file and get the name, url, icon and categories
+    while IFS= read -r line; do
+        case $line in
+            "Name="*)
+                name=${line#*Name=}
+                ;;
+            "Exec="*)
+                browser=${line%% *}
+                browser=${browser#Exec=}
+                if [[ "$browser" =~ biglinux-webapp ]]; then
+                    browser=$defaultBrowser
 
-			# Extract the URL and profile directory if they are not already set
-			url=${url:-${line#*--app=}}
-			profile=${profile:-${line#*--profile-directory=}}
-			profile=${profile% *}
-			;;
-		"Icon="*)
-			icon=${line#*Icon=}
-			icon=$(echo $icon | sed "s|$HOME/.local/share/icons/||g;s|~/.local/share/icons/||g")
-			;;
-		"Categories="*)
-			categories=${line#*Categories=}
-			;;
-		esac
-	done <<<$(grep -m4 -e '^Name=' -e '^Exec=' -e '^Icon=' -e '^Categories=' $file)
+                # In older versions of biglinux-webapp, Firefox and browsers based in Firefox use script in ~/.local/bin
+                elif [[ "$browser" =~ \.local/bin ]]; then
+                    oldScript=$browser
+                    browser=$(grep -m1 '\--new-instance' "$oldScript" | grep -o '^[^ ]*')
+                    url=$(grep -m1 '\--new-instance' "$oldScript" | grep -o 'http[^" ]*')
+                    profile='Default'
+                fi
 
-	# Create a new desktop file compatible with wayland
-	big-webapps create "$browser" "$name" "$url" "$icon" "$categories" "$profile"
+                # Extract the URL and profile directory if they are not already set
+                url=${url:-${line#*--app=}}
+                profile=${profile:-${line#*--profile-directory=}}
+                profile=${profile% *}
+                ;;
+            "Icon="*)
+                icon=${line#*Icon=}
+                icon=$(echo $icon | sed "s|$HOME/.local/share/icons/||g;s|~/.local/share/icons/||g")
+                ;;
+            "Categories="*)
+                categories=${line#*Categories=}
+                ;;
+        esac
+    done <<<$(grep -m4 -e '^Name=' -e '^Exec=' -e '^Icon=' -e '^Categories=' $file)
 
-	# Remove the old desktop file
-	rm $file
+    # Create a new desktop file compatible with wayland
+    big-webapps create "$browser" "$name" "$url" "$icon" "$categories" "$profile"
+
+    # Remove the old desktop file
+    rm $file
 
 done
